5fe79d2c71d3f70ac52b9c7682b4cacecb3ce41f
zloydanny <dan.abdrakhimov@yandex.ru>
2018-02-27 11:43:24 +0300

    Add files via upload

--- L1-intro/src/main/java/ru/track/App2.java
+++ L1-intro/src/main/java/ru/track/App2.java
@@ -2,11 +2,26 @@ package ru.track;
 
 
 import org.apache.commons.lang3.StringUtils;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+
+
 
 public class App2 {
 
-    public static void main(String[] args) {
-        String str = StringUtils.capitalize(args[0]);
-        System.out.println(str);
+        public static String url = "https://guarded-mesa-31536.herokuapp.com/track";
+        public static String name = "name";
+        public static String git = "git";
+        public static String email = "email";
+
+    public static void main(String[] args) throws Exception{
+        HttpResponse<JsonNode> r = Unirest.post(url)
+                .field(name,"Danny")
+                .field(git,"zloydanny")
+                .field(email, "dan.abdrakhimov@yandex.ru")
+                .asJson();
+
+        System.out.println(r.getBody().getObject().get("success"));
     }
 }
